0e5494525e35382c80df1776b5638fc4ce174e1b
zahraathompson871-art <zahraathompson871@gmail.com>
2026-08-07 09:18:21 +0200

    updated modal size and removed scroll

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -347,6 +347,8 @@
             t => `${t.first} ${t.last}` === name
         );
 
+        document.body.style.overflow = "hidden";
+
         if (!trainee) return;
 
         document.getElementById("modalVisual").style.backgroundImage =
@@ -409,6 +411,7 @@
 
         modal.classList.remove("show");
         modal.setAttribute("aria-hidden", "true");
+        document.body.style.overflow = "";
 
     }
 
